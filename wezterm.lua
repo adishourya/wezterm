d6b5f1607ecd3da33773be72de636ebbe03a5bee
Adi <shourya.aditya10@gmail.com>
2025-07-10 11:35:40 +0200

    added maximize split

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,7 @@ local act = wezterm.action
 -- This is where you actually apply your config choices.
 config.font = wezterm.font 'Simple Nerd Font'
 config.font_size = 20
-config.line_height = 1.8
+config.line_height = 2
 config.hide_tab_bar_if_only_one_tab = true
 
 
@@ -43,50 +43,47 @@ local split_vertical = {
 
 
 local move_pane_up = {
-  key = 'w',
-  mods = 'ALT',
+  key = 'w', mods = 'ALT',
   action = act.ActivatePaneDirection 'Up',
 }
 
 local move_pane_left = {
-  key = 'a',
-  mods = 'ALT',
+  key = 'a', mods = 'ALT',
   action = act.ActivatePaneDirection 'Left',
 }
 
 local move_pane_down = {
-  key = 's',
-  mods = 'ALT',
+  key = 's', mods = 'ALT',
   action = act.ActivatePaneDirection 'Down',
 }
 
 local move_pane_right = {
-  key = 'd',
-  mods = 'ALT',
+  key = 'd', mods = 'ALT',
   action = act.ActivatePaneDirection 'Right',
 }
 
 
 
 local resize_pane_left = {
-  key = 'h',
-  mods = 'ALT',
-  action = act.AdjustPaneSize { 'Left', 3 },
+  key = 'h', mods = 'ALT',
+  action = act.AdjustPaneSize { 'Left', 2 },
 }
 local resize_pane_down = {
-  key = 'j',
-  mods = 'ALT',
-  action = act.AdjustPaneSize { 'Down', 3 },
+  key = 'j', mods = 'ALT',
+  action = act.AdjustPaneSize { 'Down', 2 },
 }
 local resize_pane_up = {
-  key = 'k',
-  mods = 'ALT',
-  action = act.AdjustPaneSize { 'Up', 3 },
+  key = 'k', mods = 'ALT',
+  action = act.AdjustPaneSize { 'Up', 2 },
 }
 local resize_pane_right = {
-  key = 'l',
-  mods = 'ALT',
-  action = act.AdjustPaneSize { 'Right', 3 },
+  key = 'l', mods = 'ALT',
+  action = act.AdjustPaneSize { 'Right', 2 },
+}
+
+local maximize_split = {
+  key = 'z', mods = 'ALT',
+  action = act.TogglePaneZoomState,
 }
 
 
@@ -105,4 +102,5 @@ table.insert(config.keys, resize_pane_down)
 table.insert(config.keys, resize_pane_up)
 table.insert(config.keys, resize_pane_right)
 
+table.insert(config.keys, maximize_split)
 return config
