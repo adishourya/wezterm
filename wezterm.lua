8797f230ce64b58ad45edc124efd8790fa748fc9
Adi <shourya.aditya10@gmail.com>
2025-07-10 11:22:13 +0200

    init commit

---
+++ wezterm.lua
@@ -0,0 +1,108 @@
+
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+-- This is where you actually apply your config choices.
+config.font = wezterm.font 'Simple Nerd Font'
+config.font_size = 20
+config.line_height = 1.8
+config.hide_tab_bar_if_only_one_tab = true
+
+
+-- keybindings
+
+config.keys = {}
+
+for i = 1, 8 do
+  -- ALT + number to activate that tab
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'ALT',
+    action = act.ActivateTab(i - 1),
+  })
+end
+
+
+local spawn_tab =  {
+    key = 't', mods = 'ALT',
+    action = act.SpawnTab 'CurrentPaneDomain',
+  }
+
+
+local split_horizontal = {
+  key = '=', mods = 'ALT',
+  action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+}
+
+local split_vertical = {
+  key = '-', mods = 'ALT',
+  action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+}
+
+
+local move_pane_up = {
+  key = 'w',
+  mods = 'ALT',
+  action = act.ActivatePaneDirection 'Up',
+}
+
+local move_pane_left = {
+  key = 'a',
+  mods = 'ALT',
+  action = act.ActivatePaneDirection 'Left',
+}
+
+local move_pane_down = {
+  key = 's',
+  mods = 'ALT',
+  action = act.ActivatePaneDirection 'Down',
+}
+
+local move_pane_right = {
+  key = 'd',
+  mods = 'ALT',
+  action = act.ActivatePaneDirection 'Right',
+}
+
+
+
+local resize_pane_left = {
+  key = 'h',
+  mods = 'ALT',
+  action = act.AdjustPaneSize { 'Left', 3 },
+}
+local resize_pane_down = {
+  key = 'j',
+  mods = 'ALT',
+  action = act.AdjustPaneSize { 'Down', 3 },
+}
+local resize_pane_up = {
+  key = 'k',
+  mods = 'ALT',
+  action = act.AdjustPaneSize { 'Up', 3 },
+}
+local resize_pane_right = {
+  key = 'l',
+  mods = 'ALT',
+  action = act.AdjustPaneSize { 'Right', 3 },
+}
+
+
+table.insert(config.keys,spawn_tab)
+
+table.insert(config.keys,split_horizontal)
+table.insert(config.keys,split_vertical)
+
+table.insert(config.keys, move_pane_up)
+table.insert(config.keys, move_pane_left)
+table.insert(config.keys, move_pane_down)
+table.insert(config.keys, move_pane_right)
+
+table.insert(config.keys, resize_pane_left)
+table.insert(config.keys, resize_pane_down)
+table.insert(config.keys, resize_pane_up)
+table.insert(config.keys, resize_pane_right)
+
+return config
