78a565de82e5c280274941a93557c6d018120d6d
Adi <shourya.aditya10@gmail.com>
2025-07-12 09:56:28 +0200

    no padding

--- wezterm.lua
+++ wezterm.lua
@@ -6,9 +6,20 @@ local act = wezterm.action
 
 -- This is where you actually apply your config choices.
 config.font = wezterm.font 'Simple Nerd Font'
-config.font_size = 20
+config.font_size = 21
 config.line_height = 2
 config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+
+config.show_new_tab_button_in_tab_bar = false
+-- config.tab_max_width = 99
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 
 -- keybindings
